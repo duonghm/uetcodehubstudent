34ab90a31d655d2bffc48e4f839496fb5a0a3cb4
ngxson <thichthat@gmail.com>
2016-10-19 19:11:26 +0700

    welcome: new UI

--- app/CustomClass/Statistic.php
+++ app/CustomClass/Statistic.php
@@ -9,6 +9,8 @@
 namespace App\CustomClass{
 
     use Illuminate\Support\Facades\DB;
+	use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Support\Facades\Auth;
 
     class Statistic{
 
@@ -76,6 +78,79 @@ namespace App\CustomClass{
                 )
             );
         }*/
+		
+		public function getHardestTable(){
+            return DB::select(
+                DB::raw(
+                    'SELECT problemId, numOfUser, numOfFinishedUser, ratio FROM
+						(SELECT tab1.problemId, tab1.numOfUser, tab2.numOfFinishedUser, COALESCE((tab2.numOfFinishedUser/tab1.numOfUser),-1) as ratio
+						FROM (select problemId, count(userId) as numOfUser from(
+											select problems.problemId, submissions.userId, max(submissions.resultScore) as userScore, submissions.courseId, problems.defaultScore
+											from problems left join submissions on problems.problemId = submissions.problemId
+											group by problems.problemId, submissions.userId) as s
+										group by problemId) as tab1
+						LEFT OUTER JOIN (select problems.problemId as problemId, count(s.userId) as numOfFinishedUser from(
+											select problems.problemId, submissions.userId, max(submissions.resultScore) as userScore, submissions.courseId, problems.defaultScore
+											from problems left join submissions on problems.problemId = submissions.problemId
+											group by problems.problemId, submissions.userId having userScore = defaultScore) as s
+											right join problems on s.problemId = problems.problemId
+											group by problems.problemId) as tab2
+						ON tab1.problemId=tab2.problemId) as mytab
+					WHERE mytab.ratio > 0
+					ORDER BY ratio'
+                )
+            );
+        }
+		
+		public function getCourseTable(){
+            return DB::select(
+                DB::raw(
+                    'SELECT tab1.courseId, tab2.courseName, tab1.numOfProblems FROM
+						(SELECT courseId, COUNT(courseId) AS numOfProblems
+						FROM courseproblems
+						GROUP BY courseId) AS tab1
+					LEFT JOIN
+						(SELECT courseId, courseName
+						FROM courses) AS tab2
+					ON tab1.courseId = tab2.courseId'
+                )
+            );
+        }
+		
+		public function getNumOfUnsubmit(){
+			$row = DB::select(
+				DB::raw(
+'SELECT COUNT(probId) AS result FROM
+	(SELECT probId, tab1.courseId, isActive
+	FROM (SELECT tab1.courseId, tab1.problemId AS probId, isAct  FROM
+					(SELECT courseproblems.courseId, problemId, isAct
+					FROM courseproblems
+					LEFT JOIN (SELECT courseId, isActive as isAct
+						FROM courses
+						WHERE isActive = 1) AS coursesTab
+					ON courseproblems.courseId = coursesTab.courseId
+					WHERE courseproblems.isActive = 1 AND isAct IS NOT NULL
+					) AS tab1
+				LEFT JOIN
+					(SELECT courseId, userId
+					FROM courseusers
+					WHERE userId = '.Auth::user()->userId.'
+					) AS tab2
+				ON tab1.courseId = tab2.courseId
+				WHERE userId IS NOT NULL
+		) AS tab1
+	LEFT JOIN (SELECT problemId, userId, isActive FROM submissions
+		WHERE userId = '.Auth::user()->userId.'
+		GROUP BY problemId)
+		AS tab2
+	ON probId = tab2.problemId
+	WHERE userId IS NULL
+	GROUP BY tab2.problemId)
+AS mytab'
+				)
+			);
+			return ($row[0]->result);
+		}
 
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,18 +20,40 @@
         $('#btn').click(function () {
             toastr.success("Gnome & Growl type non-blocking notifications", "Toastr Notifications");
         });
+		
+		var num_of_anim = 10;
+		var i,del=100;
+		for (i=0; i<=num_of_anim; i++) {
+			del += 100;
+			$($(".animate")[i]).hide();
+			$($(".animate")[i]).delay(del).fadeIn();
+		}
     </script>
 @endsection
 
 @section('pageTitle')
-    Homepage
+    <?php 
+	date_default_timezone_set("Asia/Bangkok");
+	$hour = intval(date('H'));
+		if ($hour > 22) {
+			$greating = "Good night";
+		} else if ($hour > 12) {
+			$greating = "Good afternoon";
+		} else if ($hour > 4) {
+			$greating = "Good morning";
+		} else {
+			$greating = "Goog night";
+		} 
+		?>
+	{{$greating}}@if(!Auth::guest()), {{Auth::user()->getFullname()}}@endif!
 @endsection
 
 @section('content')
-    @if(!Auth::guest())
         <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="dashboard-stat blue">
+    @if(!Auth::guest())
+
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat blue" id="anim1">
                     <div class="visual">
                         <i class="fa fa-comments"></i>
                     </div>
@@ -46,8 +68,8 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="dashboard-stat red">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat red" id="anim2">
                     <div class="visual">
                         <i class="fa fa-bar-chart-o"></i>
                     </div>
@@ -63,8 +85,8 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="dashboard-stat green">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green" id="anim3">
                     <div class="visual">
                         <i class="fa fa-shopping-cart"></i>
                     </div>
@@ -79,16 +101,15 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="dashboard-stat purple">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat purple" id="anim4">
                     <div class="visual">
                         <i class="fa fa-globe"></i>
                     </div>
                     <div class="details">
                         <div class="number">
-                            In maintain
-                            {{--<span data-counter="counterup" data-value="{{Auth::user()->currentRanking()}}">89</span>--}}
-                            {{--<span data-counter="counterup" data-value="">Maintain</span>--}}
+
+                            <span>In Maintain</span>
                         </div>
                         <div class="desc"> Ranking</div>
                     </div>
@@ -97,11 +118,43 @@
                     </a>
                 </div>
             </div>
-        </div>
+			<div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green-haze" id="anim5">
+                    <div class="visual">
+                        <i class="fa fa-globe"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                            <span data-counter="counterup" data-value="{{$statistic->getNumOfUnsubmit()}}">0</span>
+                        </div>
+                        <div class="desc"> Unsubmitted Problems</div>
+                    </div>
+                    <a class="more" href="javascript:;"> TRY ME NOW!
+                        <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div>
+			<!--div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat yellow-casablanca" id="anim6">
+                    <div class="visual">
+                        <i class="fa fa-globe"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                            <span data-counter="counterup" data-value="1">0</span>
+                        </div>
+                        <div class="desc"> New exam</div>
+                    </div>
+                    <a class="more" href="javascript:;"> View more
+                        <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div-->
     @else
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 margin-bottom-10">
-                <div class="dashboard-stat blue">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12 margin-bottom-10">
+                <div class="dashboard-stat blue" id="anim1">
                     <div class="visual">
                         <i class="fa fa-briefcase fa-icon-medium"></i>
                     </div>
@@ -114,8 +167,8 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                <div class="dashboard-stat red">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat red" id="anim2">
                     <div class="visual">
                         <i class="fa fa-shopping-cart"></i>
                     </div>
@@ -128,8 +181,8 @@
                     </a>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                <div class="dashboard-stat green">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green" id="anim3">
                     <div class="visual">
                         <i class="fa fa-group fa-icon-medium"></i>
                     </div>
@@ -143,49 +196,152 @@
                 </div>
             </div>
         </div>
-
+@endif
+@if(Auth::guest())
         <div class="row">
-            <div class="col-md-4 col-sm-4">
+            <div class="animate col-lg-4 col-md-4 col-sm-6 col-xs-12" id="anim4">
                 <div class="portlet light ">
                     <div class="portlet-title">
                         <div class="caption">
                             <i class="icon-share font-blue"></i>
-                            <span class="caption-subject font-blue bold uppercase">Some Courses</span>
+                            <span class="caption-subject font-blue bold uppercase">LOGIN NOW</span>
                         </div>
                     </div>
                     <div class="portlet-body">
-                        <div class="slimScrollDiv"
-                             style="position: relative; overflow: hidden; width: auto; height: 300px;">
-                            <div class="scroller" style="height: 300px; overflow: hidden; width: auto;"
-                                 data-always-visible="1" data-rail-visible="0" data-initialized="1">
-                                <div class="table-responsive">
-                                    <table class="table table-hover table-light">
-                                        <thead>
-                                        <tr>
-                                            <th> Course Name </th>
-                                            <th> Semester </th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php $courses = $statistic->getTopCourses()?>
-                                        @foreach($courses as $course)
-                                        <tr>
-                                            <td>{{$course->courseName}}</td>
-                                            <td>{{$course->Semester->semesterName}}</td>
-                                        </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+	<form class="login-form" role="form" method="POST" action="{{ url('/login') }}">
+        {!! csrf_field() !!}
+        <div class="alert alert-danger display-hide">
+            <button class="close" data-close="alert"></button>
+            <span> Enter any username and password. </span>
+        </div>
+
+        @if ($errors->has('username'))
+        <div class="alert alert-danger">
+            <span><strong>{{ $errors->first('username') }}</strong></span>
+        </div>
+        @endif
+        @if ($errors->has('password'))
+        <div class="alert alert-danger">
+            <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+        </div>
+        @endif
+
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Username</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off"
+                   placeholder="Username" name="username"/></div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Password</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off"
+                   placeholder="Password" name="password"/>
+        </div>
+		<input type="checkbox" name="remember" value="1"/>Remember </label><br/><br/>
+        <div class="form-actions">
+            <center><button type="submit" class="btn green uppercase" style="width:100%"
+				data-toggle="modal" data-target="#enroll-modal">Login</button></center>
+            <label class="rememberme check">
+        </div>
+        <div class="modal fade" id="enroll-modal" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close"
+                                data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Please wait...</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Please wait...</p>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="form-group">
+							
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
 
                     </div>
                 </div>
             </div>
+			<div class="animate col-lg-8 col-md-8 col-sm-6 col-xs-12" id="anim5">
+                <div class="portlet light ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="icon-share font-blue"></i>
+                            <span class="caption-subject font-blue bold uppercase">Course Info.</span>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
+						<?php $coursetable = $statistic->getCourseTable() ?>
+                        <div class="scroller" style="height: 210px;" data-always-visible="1"
+                             data-rail-visible1="0" data-handle-color="#D7DCE2">
+                            <table class="table table-hover table-light">
+                                <thead>
+                                <tr class="uppercase">
+                                    <th width="80%"> Course name<br/><br/></th>
+                                    <th width="20%"> Num. Of Problems</th>
+                                </tr>
+                                </thead>
+                                @foreach($coursetable as $course)
+                                    <tr>
+                                        <td> {{$course->courseName}}</td>
+                                        <td> {{$course->numOfProblems}}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+@endif
+
+@if(!Auth::guest())
+			<div class="animate col-lg-8 col-md-8 col-sm-6 col-xs-12" id="anim5">
+                <div class="portlet light ">
+                    <div class="portlet-title">
+                        <div class="caption">
+                            <i class="icon-share font-blue"></i>
+                            <span class="caption-subject font-blue bold uppercase">TOP HARDEST PROBLEMS</span>
+                        </div>
+                    </div>
+                    <div class="portlet-body">
+										<?php $hardestTable = $statistic->getHardestTable(); $countProbleme = 1; ?>
+                                        <div class="scroller" style="height: 210px;" data-always-visible="1"
+                                             data-rail-visible1="0" data-handle-color="#D7DCE2">
+                                            <table class="table table-hover table-light">
+                                                <thead>
+                                                <tr class="uppercase">
+                                                    <th width="10%"></th>
+                                                    <th width="80%"> Name</th>
+                                                    <th width="10%"> Ratio</th>
+                                                </tr>
+                                                </thead>
+                                                @foreach($hardestTable as $probleme)
+                                                    <tr>
+                                                        <td>
+                                                            <a href="javascript:;"
+                                                               class="primary-link">{{$countProbleme}}</a>
+                                                        </td>
+                                                        <td> {{$probleme->problemId}}</td>
+                                                        <td>
+                                                            {{$probleme->ratio}}
+                                                        </td>
+                                                    </tr>
+												<?php $countProbleme = $countProbleme + 1; ?>
+                                                @endforeach
+                                            </table>
+                                        </div>
+                    </div>
+                </div>
+            </div>
+@endif
 
         </div>
-
-    @endif
+		<script src="{{URL::asset('assets/pages/scripts/login.min.js')}}" type="text/javascript"></script>
 
 @endsection
