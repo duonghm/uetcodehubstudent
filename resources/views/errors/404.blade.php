5068c32b904c972677b35d70e5058fcfc3fc231f
lekhanhtrinh <trinhlk@vnu.edu.vn>
2016-11-17 09:20:00 +0700

    errors: show 404 and 500

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1><b>Error 404: Page not found!</b></h1>
+	<h3><a href="{{url('/')}}">Go back to home</a></h3>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+	<h1><b>Error 500: Internal server error!</b></h1>
+	<h3><a href="{{url('/')}}">Go back to home</a></h3>
+@endsection
