f4e4f47878dcf5261cdc826016d65a03ac29c5e9
ngxson <thichthat@gmail.com>
2016-12-17 11:13:04 +0700

    showProblems.blade: fix responsive table

--- resources/views/course/showProblems.blade.php
+++ resources/views/course/showProblems.blade.php
@@ -5,6 +5,7 @@
 {{--@endsection--}}
 
 @section('extendedHead')
+	<link rel='stylesheet prefetch' href="{{url('/css/responsive-table.css')}}">
 @endsection
 
 @section('script')
@@ -36,66 +37,66 @@
                 <div class="table-scrollable table-scrollable-borderless">
                     <div style="text-align: center">{!! $problems->render() !!}</div>
 					<section id="no-more-tables">
-                    <table class="table-bordered table-striped table-condensed table table-hover table-light">
-                        <thead>
-							<tr>
-								<th width="10%">STT</th>
-								{{--<th>Tag value</th>--}}
-								{{--<th>Độ khó</th>--}}
-								<th width="30%">Mã bài</th>
-								<th width="10%">Điểm tối đa</th>
-								<th width="10%">Đã nộp</th>
-								<th width="10%">Đã hoàn thành</th>
-								<th width="20%">Điểm hiện tại</th>
-							</tr>
-						</thead>
-                        <tbody>
-                        @foreach($problems as $index=>$p)
-							@php
-								$startIndex = ($problems->currentPage()-1) * $problems->perPage();
-								$problemscore = $p->getScoreOfUser($course->courseId);
-								$problemsolveresult = $p->getProblemSolvingResult($p->pivot->courseProblemId);
-								if (sizeof($problemsolveresult) == 0) {
-									$submitted = 'Updating';
-									$finished = 'Updating';
-								} else {
-									$submitted = $problemsolveresult[0]->submittedUser.' người';
-									$finished = $problemsolveresult[0]->finishedUser.' người';
-								}
-							@endphp
-                            <tr class="animate">
-                                @php
-                                    $startIndex = ($problems->currentPage()-1) * $problems->perPage();
-                                @endphp
-                                <td data-title="STT">
-									<a href="{{url(Request::path().'/'.$p->problemId)}}">Bài {{$problems->total() - $startIndex - $index}}</a>
-								</td>
-									{{--<td width="300px">{{$p->tagValues}}</td>--}}
-									{{--<td>{{$p->pivot->hardLevel}}</td>--}}
-                                <td data-title="Mã bài">{{$p->problemCode}}</td>
-                                <td data-title="Điểm tối đa">{{$p->defaultScore}}</td>
-                                <td data-title="Đã nộp">{{$submitted}}</td>
-                                @php
-                                    $problemScore = $p->getScoreOfUser($course->courseId);
-                                @endphp
-                                <td data-title="Đã hoàn thành">
-                                    {{$finished}}
-                                </td>
-                                <td data-title="Điểm hiện tại">
-                                    <button type="button"
-										onclick="document.location = '{{url(Request::path().'/'.$p->problemId)}}?page={{$problems->currentPage()}}';"
-									@if($problemscore == null)
-										class="btn btn-primary" > <b>LÀM BÀI NÀY</b> </button>
-									@elseif($problemscore == $p->defaultScore)
-										class="btn btn-success" > <i class="fa fa-check"></i> <b>HOÀN THÀNH</b></button>
-									@else
-										class="btn btn-warning" style="color: #000;"> <b>THỬ LẠI</b> ({{$problemscore}}) </button>
-									@endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+						<table class="table-bordered table-striped table-condensed table table-hover table-light">
+							<thead>
+								<tr>
+									<th width="10%">STT</th>
+									{{--<th>Tag value</th>--}}
+									{{--<th>Độ khó</th>--}}
+									<th width="30%">Mã bài</th>
+									<th width="10%">Điểm tối đa</th>
+									<th width="10%">Đã nộp</th>
+									<th width="10%">Đã hoàn thành</th>
+									<th width="20%">Điểm hiện tại</th>
+								</tr>
+							</thead>
+							<tbody>
+							@foreach($problems as $index=>$p)
+								@php
+									$startIndex = ($problems->currentPage()-1) * $problems->perPage();
+									$problemscore = $p->getScoreOfUser($course->courseId);
+									$problemsolveresult = $p->getProblemSolvingResult($p->pivot->courseProblemId);
+									if (sizeof($problemsolveresult) == 0) {
+										$submitted = 'Updating';
+										$finished = 'Updating';
+									} else {
+										$submitted = $problemsolveresult[0]->submittedUser.' người';
+										$finished = $problemsolveresult[0]->finishedUser.' người';
+									}
+								@endphp
+								<tr class="animate">
+									@php
+										$startIndex = ($problems->currentPage()-1) * $problems->perPage();
+									@endphp
+									<td data-title="STT">
+										<a href="{{url(Request::path().'/'.$p->problemId)}}">Bài {{$problems->total() - $startIndex - $index}}</a>
+									</td>
+										{{--<td width="300px">{{$p->tagValues}}</td>--}}
+										{{--<td>{{$p->pivot->hardLevel}}</td>--}}
+									<td data-title="Mã bài">{{$p->problemCode}}</td>
+									<td data-title="Điểm tối đa">{{$p->defaultScore}}</td>
+									<td data-title="Đã nộp">{{$submitted}}</td>
+									@php
+										$problemScore = $p->getScoreOfUser($course->courseId);
+									@endphp
+									<td data-title="Đã hoàn thành">
+										{{$finished}}
+									</td>
+									<td data-title="Điểm hiện tại">
+										<button type="button"
+											onclick="document.location = '{{url(Request::path().'/'.$p->problemId)}}?page={{$problems->currentPage()}}';"
+										@if($problemscore == null)
+											class="btn btn-primary" > <b>LÀM BÀI NÀY</b> </button>
+										@elseif($problemscore == $p->defaultScore)
+											class="btn btn-success" > <i class="fa fa-check"></i> <b>HOÀN THÀNH</b></button>
+										@else
+											class="btn btn-warning" style="color: #000;"> <b>THỬ LẠI</b> ({{$problemscore}}) </button>
+										@endif
+									</td>
+								</tr>
+							@endforeach
+							</tbody>
+						</table>
 					</section>
                     <div style="text-align: center">{!! $problems->render() !!}</div>
                 </div>
